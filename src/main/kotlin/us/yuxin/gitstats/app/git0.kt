9b7e236100c45ee99f74e77887bc741435d7de73
Xin Yu <scaner@gmail.com>
2016-01-13 18:32:01 +0800

    more kotlin sample case

---
+++ src/main/kotlin/us/yuxin/gitstats/app/git0.kt
@@ -0,0 +1,7 @@
+package us.yuxin.gitstats.app.git0;
+
+
+
+fun main(args: Array<String>) {
+
+}
--- src/main/kotlin/us/yuxin/gitstats/hello.kt
+++ src/main/kotlin/us/yuxin/gitstats/hello.kt
@@ -1,3 +1,5 @@
+package us.yuxin.gitstats;
+
 
 fun main(args: Array<String>) {
   println("Hello Worlds")
