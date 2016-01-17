83727f0a4b29ae987933adef57a395571c9d5fd4
Xin Yu <scaner@gmail.com>
2016-01-17 23:39:23 +0800

    Keep on analyze module. not finished

--- src/main/java/us/yuxin/gitstats/analyze.kt
+++ src/main/java/us/yuxin/gitstats/analyze.kt
@@ -1,36 +1,98 @@
 package us.yuxin.gitstats
 
 import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.diff.DiffEntry
+import org.eclipse.jgit.diff.DiffFormatter
+import org.eclipse.jgit.lib.Repository
+import org.eclipse.jgit.revwalk.RevCommit
+import org.eclipse.jgit.treewalk.CanonicalTreeParser
 import java.util.*
 
 
 data class Commit(
   val id:String,
   val author: String,
-  val parents:String,
-  val messages:String,
+  val parent:String,
+  val merge:String?,
+  val message:String,
   val lineAdded:Int,
   val lineModified:Int,
   val lineDeleted:Int,
   val binaryAdded:Int,
   val binaryModified:Int,
   val binaryDeleted:Int,
-  val merge:Boolean = false,
-  val changes:List<Change>
+  val changes:List<Change>?
 )
+
 
 data class Change(
   val path:String,
+  val type:Int,
   val section:Int,
   val lineAdded:Int,
   val lineModified:Int,
   val lineDeleted:Int
 )
 
+private fun analyzeDiff(repo:Repository, diff:DiffEntry):Change {
+  return Change(
+    path = diff.newPath,
+    type = diff.changeType.ordinal,
+    section = 0,
+    lineAdded = 0,
+    lineModified = 0,
+    lineDeleted = 0)
+}
+
+
+fun analyzeRev(repo:Repository, rev:RevCommit):Commit {
+  val git = Git(repo)
+  val revId = rev.id
+  val parents = rev.parents
+
+  val id_ = revId.toString()
+  val parent_ = parents[0].id.toString()
+  val author_ = rev.authorIdent.emailAddress
+
+  val merge_ = if (parents.size == 2) {
+    parents[1].id.toObjectId().toString()
+  } else {
+    null
+  }
+
+  val objectReader = repo.newObjectReader()
+
+  val newTreeIter = CanonicalTreeParser()
+  newTreeIter.reset(objectReader, rev.tree)
+  val oldTreeIter = CanonicalTreeParser()
+  oldTreeIter.reset(objectReader, rev.parents[0].tree)
+
+  val diffs = git.diff()
+    .setNewTree(newTreeIter)
+    .setOldTree(oldTreeIter)
+    .call()
+
+  val changes_ = diffs.map {analyzeDiff(repo, it)}
+
+  return Commit(
+    id = id_,
+    author = author_,
+    parent = parent_,
+    merge = merge_,
+    message = rev.fullMessage,
+    lineAdded = 0,
+    lineDeleted = 0,
+    lineModified = 0,
+    binaryAdded = 0,
+    binaryDeleted = 0,
+    binaryModified = 0,
+    changes = changes_)
+}
+
 
 fun analyzeRepository(
   c:GSConfig.Root, config:GSConfig.Repository,
-  commitCache:Map<String, Commit>? = null):Unit {
+  commitCache:MutableMap<String, Commit>? = null):Unit {
 
   val cache = commitCache ?: HashMap<String, Commit>()
 
@@ -39,14 +101,15 @@ fun analyzeRepository(
   println(repo)
 
   val logc = git.log()
-  val branches = config.branches ?: "origin/master"
 
-  for (branch in branches.split(Regex("[\\s,]+"))) {
-    logc.add(repo.resolve("refs/remotes/" + branch))
-  }
-  val logs = logc.call()
+  val logs = logc.all().call()
   for (rev in logs) {
-    println(rev.name + ":" + rev.shortMessage)
+    if (rev.id.toString() in cache) {
+      continue
+    }
+
+    val commit = analyzeRev(repo, rev)
+    cache[commit.id] = commit
   }
 }
 
